1181020b131f59f9427a149435de321bebf5ef86
jayherjay <jayherjay@gmail.com>
2023-01-31 14:49:03 +0300

    Hot Fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-
-"""
-    Recursive function that queries the Reddit API and returns a list
-    containing the titles of all hot articles for a given subreddit.
-"""
-
-import requests
+"""Module for task 2"""
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """Querying Reddit API, and returns all
-    hot articles for a given subreddit."""
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """Queries the Reddit API and returns all hot posts
+    of the subreddit"""
+    import requests
 
-    url = "https://api.reddit.com/r/{}/hot".format(subreddit)
-    headers = {'User-Agent': 'Carlos Barros'}
-    arg = {"after": after}
-    resp = requests.get(url, params=arg, headers=headers)
-    list_a = resp.json().get('data', {}).get('children', None)
-    pagination = resp.json().get('data', {}).get('after', None)
-
-    if pagination is not None:
-
-        if list_a:
-            for item in list_a:
-                hot_list.append(item.get("data").get("title"))
-
-        if pagination is not None:
-            recurse(subreddit, hot_list, pagination)
-
-        return hot_list
-
-    else:
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"count": count, "after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 400:
         return None
+
+    hot_l = hot_list + [child.get("data").get("title")
+                        for child in sub_info.json()
+                        .get("data")
+                        .get("children")]
+
+    info = sub_info.json()
+    if not info.get("data").get("after"):
+        return hot_l
+
+    return recurse(subreddit, hot_l, info.get("data").get("count"),
+                   info.get("data").get("after"))
